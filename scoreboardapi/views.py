0fd958c24ff3f90eefe9ca511ecf2e6344b32935
Mohamad Dolatshah <dolatshah.mohammad@gmail.com>
2020-11-29 19:01:32 -0800

    handle errors

--- scoreboardapi/views.py
+++ scoreboardapi/views.py
@@ -4,66 +4,72 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from .serializers import PlayerSerializer
 from .models import Player
+from django.http import JsonResponse,HttpResponseBadRequest
 
 from rest_framework.exceptions import ValidationError, ParseError
 
 
 class PlayerViewSet(viewsets.ModelViewSet):
 	"""
-    retrieve:
-        Returns a list of all **active** players in the database. For more details on how players are created please see here [ref]. [ref]: http://localhost:8000/players
+	retrieve:
+		Returns a list of all **active** players in the database. For more details on how players are created please see here [ref]. [ref]: http://localhost:8000/players
 
-    list:
-        Return all players, ordered by score.
+	list:
+		Return all players, ordered by score.
 
-    create:
-        Create a new player.
+	create:
+		Create a new player.
 
-    delete:
-        Remove an existing player.
+	delete:
+		Remove an existing player.
 
-    partial_update:
-        Update one or more fields on an existing player.
-        http://localhost:8000/players/{id}
+	partial_update:
+		Update one or more fields on an existing player.
+		http://localhost:8000/players/{id}
 
-    update:
-        Update a player.
+	update:
+		Update a player.
 
-    """
+	"""
 	queryset = Player.objects.all().order_by('points')
 	serializer_class = PlayerSerializer
 
-	def patch(self, request, pk):
+	def patch(self, request, pk=None):
 		"""
-		
+		partialy update a player using id
 		"""
+		if not pk:
+			return HttpResponseBadRequest('id is missing')
 		testmodel_object = self.get_object(pk)
 		serializer = PlayerSerializer(testmodel_object, data=request.data, partial=True) # set partial=True to update a data partially
 		if serializer.is_valid(): #check if the serializer is valid
 			serializer.save()
-			return JsonResponse(code=201, data=serializer.data)
-		return JsonResponse(code=400, data="wrong parameters")
+			return JsonResponse(data=serializer.data,safe=False)
+		return HttpResponseBadRequest('wrong parameters')
 
 
 @api_view(['POST'])
-def pointsUpdate(request, pk):
+def pointsUpdate(request, pk=None):
 	"""
 	Update score of a player
 
 	{
-	    "action" : "plus"
+		"action" : "plus"
 	}
 
 	or 
 
 	{
-	    "action" : "minus"
+		"action" : "minus"
 	}
 
 	[ref]
 
 	[ref]: http://localhost:8000/pointsUpdate/<id>/
-    """
+	"""
+	if not pk:
+		return HttpResponseBadRequest('id is missing')
+
 	player = Player.objects.get(id=pk)
 
 	serializer = PlayerSerializer(instance=player)
@@ -79,7 +85,7 @@ def pointsUpdate(request, pk):
 		else:
 			raise ValidationError
 	else:
-		return JsonResponse(code=400, data="wrong parameters")
+		return  HttpResponseBadRequest('wrong parameters')
 
 	
 	player.save()
